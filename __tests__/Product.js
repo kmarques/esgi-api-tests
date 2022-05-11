63a44215c4ae1c4bd12780ff12b88dc450a11ca6
Karl MARQUES <kmarques@vetixy.com>
2022-05-11 08:38:15 +0200

    feat: tests in transactions

--- __tests__/Product.js
+++ __tests__/Product.js
@@ -3,6 +3,18 @@ const app = require("../app.js");
 const { sequelize } = require("../models");
 const request = supertest(app);
 
+beforeAll(async () => {
+  sequelize.constructor._cls = new Map();
+});
+
+beforeEach(async () => {
+  const trx = await sequelize.transaction();
+  sequelize.constructor._cls.set("transaction", trx);
+});
+afterEach(async () => {
+  await sequelize.constructor._cls.get("transaction").rollback();
+});
+
 afterAll(() => {
   sequelize.close();
 });
